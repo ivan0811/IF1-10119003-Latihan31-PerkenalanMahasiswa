82831dd26915627acd6efaae5cef678bcbe377d5
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-11-07 16:32:24 +0700

    initial commit

--- src/com/ivanfaathirza/perkenalanmahasiswa/IF110119003Latihan31Perkenalan.java
+++ src/com/ivanfaathirza/perkenalanmahasiswa/IF110119003Latihan31Perkenalan.java
@@ -1,5 +1,14 @@
 package com.ivanfaathirza.perkenalanmahasiswa;
 
+/**
+ *
+ * @author Ivan
+ * NAMA         : Ivan Faathirza
+ * KELAS        : IF1
+ * NIM          : 10119003
+ * Deskripsi Program : program ini untuk menampilkan perkenalan mahasiswa berdasarkan konsep objek dan instansiasi objek
+ */
+
 public class IF110119003Latihan31Perkenalan {
 
     public static void main(String[] args) {
